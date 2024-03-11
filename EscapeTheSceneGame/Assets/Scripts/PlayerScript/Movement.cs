08a3c0f4f99ef8fcc0b5c19aa911d94839170774
Jason <JPriskomatis@gmail.com>
2024-03-11 23:23:33 +0200

    Added movement + camera script

--- EscapeTheSceneGame/Assets/Scripts/PlayerScript/Movement.cs
+++ EscapeTheSceneGame/Assets/Scripts/PlayerScript/Movement.cs
@@ -1,38 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotationSpeed = 10f;
+    
+    [SerializeField] Transform playerCamera;
+    [SerializeField] [Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
+    [SerializeField] bool cursorLock = true;
+    [SerializeField] float mouseSensitivity = 3.5f;
+    [SerializeField] float Speed = 6.0f;
+    [SerializeField] [Range(0.0f, 0.5f)] float moveSmoothTime = 0.3f;
+    [SerializeField] float gravity = -30f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
 
-    private Rigidbody rb;
+    public float jumpHeight = 6f;
+    float velocityY;
+    bool isGrounded;
+
+    float cameraCap;
+    Vector2 currentMouseDelta;
+    Vector2 currentMouseDeltaVelocity;
+
+    CharacterController controller;
+    Vector2 currentDir;
+    Vector2 currentDirVelocity;
+    Vector3 velocity;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        // Freeze rotation so the player doesn't tip over
-        rb.freezeRotation = true;
+        controller = GetComponent<CharacterController>();
+
+        if (cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
+        }
     }
 
     void Update()
     {
-        // Get input for movement
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        UpdateMouse();
+        UpdateMove();
+    }
 
-        // Calculate movement direction
-        Vector3 movement = new Vector3(horizontal, 0f, vertical);
-        movement.Normalize(); // Ensure diagonal movement isn't faster
+    void UpdateMouse()
+    {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
-        // Rotate towards movement direction
-        if (movement != Vector3.zero)
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+
+        cameraCap -= currentMouseDelta.y * mouseSensitivity;
+
+        cameraCap = Mathf.Clamp(cameraCap, -90.0f, 90.0f);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraCap;
+
+        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
+    }
+
+    void UpdateMove()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, ground);
+
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDir.Normalize();
+
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+
+        if (isGrounded)  // Reset velocityY when grounded
         {
-            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+            velocityY = -0.001f;  // You can set it to a small negative value to keep the player grounded
+        }
+        else
+        {
+            velocityY += gravity * 2f * Time.deltaTime;
         }
 
-        // Move the player using Rigidbody
-        Vector3 moveDirection = transform.TransformDirection(movement) * speed;
-        rb.velocity = new Vector3(moveDirection.x, rb.velocity.y, moveDirection.z);
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * Speed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocityY = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (isGrounded && controller.velocity.y < -1f)
+        {
+            velocityY = -8f;
+        }
     }
+
 }
