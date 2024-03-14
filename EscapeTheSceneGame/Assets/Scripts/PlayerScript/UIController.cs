3a7d813882da78c2f07c7997331e2b86b327d2dc
Jason <JPriskomatis@gmail.com>
2024-03-14 21:50:02 +0200

    Added book system

---
+++ EscapeTheSceneGame/Assets/Scripts/PlayerScript/UIController.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum GameState
+{
+    Playing,
+    Paused,
+    ReadingBook
+}
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance; // Singleton instance
+
+    private GameState currentState = GameState.Playing;
+
+    public bool canMove;
+    void Awake()
+    {
+        // Ensure only one instance of GameController exists
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PauseGame()
+    {
+        currentState = GameState.Paused; 
+        Time.timeScale = 0f; //Pause the game;
+    }
+
+    public void ResumeGame()
+    {
+        currentState = GameState.Playing;
+        Time.timeScale = 1f; //Resume normal time scale;
+    }
+
+    public void EnterReadingBookState()
+    {
+        currentState = GameState.ReadingBook;
+        canMove = false;
+        Cursor.lockState = CursorLockMode.None;
+        
+    }
+
+    public void ExitReadingBookState()
+    {
+        currentState = GameState.Playing;
+        canMove = true;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        
+    }
+
+    public bool IsGamePaused()
+    {
+        return currentState == GameState.Paused;
+    }
+
+    public bool IsReadingBook()
+    {
+        return currentState == GameState.ReadingBook;
+    }
+
+
+}
