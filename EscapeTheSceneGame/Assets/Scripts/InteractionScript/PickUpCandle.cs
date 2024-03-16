f633ae489bac4a05808c5160f4c68562024b35ae
Jason <JPriskomatis@gmail.com>
2024-03-16 19:11:27 +0200

    pickUpscript works great now + lighting environment

--- EscapeTheSceneGame/Assets/Scripts/InteractionScript/PickUpCandle.cs
+++ EscapeTheSceneGame/Assets/Scripts/InteractionScript/PickUpCandle.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PickUpCandle : MonoBehaviour
@@ -7,46 +5,48 @@ public class PickUpCandle : MonoBehaviour
     [SerializeField] private GameObject candle;
     private TextAppear textAppear;
     private HighlightEffect highlight;
-
-    private bool emissionToggled = false;
+    private bool playerInRange = false;
 
     private void Start()
     {
-        //Highlight effect;
+        // Initialize highlight effect
         highlight = GetComponent<HighlightEffect>();
-
-
         candle.SetActive(false);
         textAppear = FindObjectOfType<TextAppear>();
     }
 
-    private void OnTriggerStay(Collider other)
+    private void Update()
     {
-        if (other.gameObject.tag == "Player")
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
         {
-            if (!emissionToggled) // Check if emission has not been toggled yet
-            {
-                highlight.ToggleEmission();
-                emissionToggled = true; // Set the flag to true
-            }
+            PickUp();
+        }
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && !playerInRange)
+        {
+            playerInRange = true;
             textAppear.SetText("Press 'E' to pick up the candle.");
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                textAppear.RemoveText();
-                this.gameObject.SetActive(false);
-                candle.SetActive(true);
-            }
+            highlight.ToggleEmission(); // Enable highlight only if player just entered the range
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (emissionToggled) // Check if emission has been toggled
+        if (other.gameObject.CompareTag("Player") && playerInRange)
         {
+            playerInRange = false;
             textAppear.RemoveText();
-            highlight.ToggleEmission();
-            emissionToggled = false; // Reset the flag
+            highlight.ToggleEmission(); // Disable highlight only if player just exited the range
         }
+    }
+
+    private void PickUp()
+    {
+        candle.SetActive(true);
+        gameObject.SetActive(false);
+        textAppear.RemoveText();
     }
 }
