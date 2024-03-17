7f73f33e5cbc90d5e7c193400209a99ef5f4aefb
Jason <JPriskomatis@gmail.com>
2024-03-17 18:30:33 +0200

    Can change between items now

--- EscapeTheSceneGame/Assets/Scripts/InteractionScript/Candle.cs
+++ EscapeTheSceneGame/Assets/Scripts/InteractionScript/Candle.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class PickUpCandle : MonoBehaviour
+public class Candle : MonoBehaviour
 {
     private TextAppear textAppear;
     private HighlightEffect highlight;
--- EscapeTheSceneGame/Assets/Scripts/InteractionScript/PickUpItem.cs
+++ EscapeTheSceneGame/Assets/Scripts/InteractionScript/PickUpItem.cs
@@ -6,16 +6,29 @@ public class PickUpItem : MonoBehaviour
 {
     [SerializeField] private GameObject placeHolder;
     [SerializeField] private Transform parentObject;
+    private GameObject currentPickedUpItem;
 
     public void PickUp(GameObject item)
     {
         Debug.Log("Picking up Item");
-        GameObject pickedUpItem = Instantiate(item, placeHolder.transform.position, placeHolder.transform.rotation);
-        pickedUpItem.transform.parent = parentObject;
 
-        RemoveAllScripts(pickedUpItem);
+        // Check if there's already a picked-up item
+        if (currentPickedUpItem != null)
+        {
+            // Destroy the current picked-up item
+            Destroy(currentPickedUpItem);
+        }
+
+        // Instantiate and set the new picked-up item
+        currentPickedUpItem = Instantiate(item, placeHolder.transform.position, placeHolder.transform.rotation);
+        currentPickedUpItem.transform.parent = parentObject;
+
+        // Remove all scripts from the picked-up item
+        RemoveAllScripts(currentPickedUpItem);
     }
 
+
+    //We remove all scripts from the picked-up item;
     private void RemoveAllScripts(GameObject gameObject)
     {
         MonoBehaviour[] scripts = gameObject.GetComponents<MonoBehaviour>();
