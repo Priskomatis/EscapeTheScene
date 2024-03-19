d92d819a09f161d86359a9a1052a89bba27606b1
Jason <JPriskomatis@gmail.com>
2024-03-19 22:28:35 +0200

    reset to previous

--- EscapeTheSceneGame/Assets/Scripts/InteractionScript/DetectObject.cs
+++ EscapeTheSceneGame/Assets/Scripts/InteractionScript/DetectObject.cs
@@ -7,68 +7,99 @@ public class DetectObject : MonoBehaviour
 {
     [SerializeField] private LayerMask objectLayer;
     [SerializeField] private float distance;
-    [SerializeField] private GameObject panel;
+    private Dictionary<string, System.Action> objectActions = new Dictionary<string, System.Action>();
+    private Transform lastHitTransform;
+
+
+    [SerializeField] GameObject panel;
     [SerializeField] private TextMeshProUGUI text;
 
-    private Transform lastHitTransform;
-    private BookObject lastHitBookObject;
+
+    //private ChestManager chestManager;
+    private bool emissionToggled = false;
+
+
+    private void Start()
+    {
+        
+        // Populate the dictionary with object names and corresponding actions
+        objectActions.Add("Book", HandleBook);
+        objectActions.Add("Chest", HandleChest);
+        objectActions.Add("Door", HandleDoor);
+        objectActions.Add("Candle", HandleCandle);
+
+        //chestManager = FindObjectOfType<ChestManager>();
+    }
 
     private void Update()
     {
+        var ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(transform.position, transform.forward, out hit, distance, objectLayer))
+        if (Physics.Raycast(ray, out hit, distance, objectLayer))
         {
-            if (hit.collider != null)
+            if (hit.collider.transform != null)
             {
-                Transform hitTransform = hit.transform;
+                lastHitTransform = hit.transform;
+                string objectTag = lastHitTransform.gameObject.tag;
 
-                if (hitTransform.CompareTag("Book"))
+                // Check if the object has an action associated with it
+                if (objectActions.ContainsKey(objectTag))
                 {
-                    HandleBook(hitTransform.GetComponent<BookObject>());
+                    // Perform the action associated with the object
+                    objectActions[objectTag].Invoke();
                 }
+
             }
         }
         else
         {
-            ResetEmission();
             EraseText();
+            
         }
     }
 
-    private void HandleBook(BookObject book)
+
+    //Here we handle each function for when we are close to the gameobject;
+    private void HandleBook()
     {
         Debug.Log("Detected Book!");
 
-        // Highlight the book if it's not already highlighted
-        if (book != lastHitBookObject)
-        {
-            ResetEmission();
-            book.ToggleEmmision(true);
-            lastHitBookObject = book;
-        }
 
         // Check if the book is already open
-        if (book.GetComponent<BookObject>().isOpen)
+        if (lastHitTransform.GetComponent<BookObject>().isOpen)
         {
-            
-            book.GetComponent<BookObject>().CloseBook();
+            lastHitTransform.GetComponent<BookObject>().CloseBook();
         }
         else
         {
             // Open the book if it's closed
             IndicatorText("Press E to Read the Book");
-            book.GetComponent<BookObject>().OpenBook();
+            lastHitTransform.GetComponent<BookObject>().OpenBook();
         }
     }
 
-    private void ResetEmission()
+    private void HandleChest()
     {
-        if (lastHitBookObject != null)
-        {
-            lastHitBookObject.ToggleEmmision(false);
-            lastHitBookObject = null;
-        }
+        
+        Debug.Log("Detected Chest!");
+        IndicatorText("Press E to Open Chest");
+        lastHitTransform.GetComponent<ChestManager>().OpenChest();
+    }
+
+    private void HandleCandle()
+    {
+        Debug.Log("Detected Candle!");
+
+        IndicatorText("Press E to Pickup the Candle");
+        lastHitTransform.GetComponent<Candle>().PickUp();
+        lastHitTransform.GetComponent<Candle>().ToggleEmmision();
+    }
+    private void HandleDoor()
+    {
+        Debug.Log("Detected Door!");
+        // Example: lastHitTransform.GetComponent<DoorScript>().DoSomething();
+        
     }
 
     private void IndicatorText(string indicator)
@@ -80,5 +111,6 @@ public class DetectObject : MonoBehaviour
     private void EraseText()
     {
         panel.SetActive(false);
+
     }
 }
