b1e1d122638758d1f8fe95b529cb11a3c1799599
Jason <JPriskomatis@gmail.com>
2024-03-22 21:15:48 +0200

    Ringphone interaction

--- EscapeTheSceneGame/Assets/Scripts/Objects/Phone/PhoneScript.cs
+++ EscapeTheSceneGame/Assets/Scripts/Objects/Phone/PhoneScript.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PhoneScript : MonoBehaviour
+public class PhoneScript : MonoBehaviour, IInteractable
 {
     [SerializeField] private AudioSource audioSource;
     private bool canRing;
+
+    private TextAppear textAppear;
 
 
 
@@ -14,23 +16,29 @@ public class PhoneScript : MonoBehaviour
     {
         canRing = false;
         quest = FindObjectOfType<Quest>();
+        textAppear = FindObjectOfType<TextAppear>();
 
     }
 
-    private void Update()
+
+    public void Interact()
     {
         if (canRing && Input.GetKeyDown(KeyCode.R))
         {
             audioSource.Play();
             quest.ActivateQuest("Read the Book");
-            
+
         }
     }
-    private void OnTriggerEnter(Collider other)
+
+    public void OnInteractEnter()
     {
-        if(other.CompareTag("Player"))
-        {
-            canRing=true;
-        }
+        canRing = true;
+        textAppear.SetText("Pick up the phone");
+    }
+
+    public void OnInteractExit()
+    {
+        textAppear.RemoveText();
     }
 }
